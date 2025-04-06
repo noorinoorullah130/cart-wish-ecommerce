08fc88816ce044feb6a4ff627239caef3b68a56d
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-04-06 13:47:23 +0430

    Navbar and Hero sections are done

---
+++ src/components/Navbar/LinkWithIcon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./LinkWithIcon.css"
+
+const LinkWithIcon = ({ title, link, emoji }) => {
+    return (
+        <a href={link} className="align_center">
+            {title} <img src={emoji} className="link_emoji" />
+        </a>
+    );
+};
+
+export default LinkWithIcon;
