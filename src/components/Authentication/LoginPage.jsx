32a343988672d5164625126d6d8733cb282fb7ff
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-04-10 11:34:08 +0430

    login page added and also react-hook-form

---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import "./LoginPage.css";
+
+const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    console.log(register("name"));
+
+    const onSubmit = (formData) => console.log(formData);
+
+    return (
+        <section className="align_center form_page">
+            <form
+                className="authentication_form"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <h2>Login Form</h2>
+                <div className="form_inputs">
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            className="from_text_input"
+                            placeholder="Enter your name"
+                            {...register("name", {
+                                required: true,
+                                minLength: 3,
+                            })}
+                        />
+                        {errors.name?.type === "required" && (
+                            <em className="form_error">
+                                Please enter your name
+                            </em>
+                        )}
+                        {errors.name?.type === "minLength" && (
+                            <em className="form_error">
+                                Name should be 3 or more characters
+                            </em>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Phone Number</label>
+                        <input
+                            type="number"
+                            id="phone"
+                            className="from_text_input"
+                            placeholder="Enter your phone number"
+                            {...register("phone", { valueAsNumber: true })}
+                        />
+                    </div>
+
+                    <button type="submit" className="search_button from_submit">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default LoginPage;
