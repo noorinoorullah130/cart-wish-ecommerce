3be4fa0f1c6a76db314a34b163af5a4be8124ac1
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-04-12 07:13:34 +0430

    validation schema is done

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import "./LoginPage.css";
+
+const schema = z.object({
+    email: z
+        .string()
+        .email({ message: "Please enter valid email address" })
+        .min(3),
+    password: z
+        .string()
+        .min(8, { message: "Password should be at least 8 characters" }),
+});
 
 const LoginPage = () => {
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm();
+    } = useForm({ resolver: zodResolver(schema) });
     console.log(register("name"));
 
     const onSubmit = (formData) => console.log(formData);
@@ -22,37 +34,34 @@ const LoginPage = () => {
                 <h2>Login Form</h2>
                 <div className="form_inputs">
                     <div>
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="email">Email</label>
                         <input
-                            type="text"
-                            id="name"
+                            type="email"
+                            id="email"
                             className="from_text_input"
-                            placeholder="Enter your name"
-                            {...register("name", {
-                                required: true,
-                                minLength: 3,
-                            })}
+                            placeholder="Enter your email address"
+                            {...register("email")}
                         />
-                        {errors.name?.type === "required" && (
+                        {errors.email && (
                             <em className="form_error">
-                                Please enter your name
-                            </em>
-                        )}
-                        {errors.name?.type === "minLength" && (
-                            <em className="form_error">
-                                Name should be 3 or more characters
+                                {errors.email.message}
                             </em>
                         )}
                     </div>
                     <div>
-                        <label htmlFor="phone">Phone Number</label>
+                        <label htmlFor="password">Password</label>
                         <input
-                            type="number"
-                            id="phone"
+                            type="password"
+                            id="password"
                             className="from_text_input"
-                            placeholder="Enter your phone number"
-                            {...register("phone", { valueAsNumber: true })}
+                            placeholder="Enter your password"
+                            {...register("password")}
                         />
+                        {errors.password && (
+                            <em className="form_error">
+                                {errors.password.message}
+                            </em>
+                        )}
                     </div>
 
                     <button type="submit" className="search_button from_submit">
